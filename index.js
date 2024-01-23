75ec940d2c65e3fd96f68085f7542d83c772a4da
username <user@email.mail>
2024-01-23 23:29:04 +0900

    24년달력완성

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
-let month = prompt('현재는 몇 월인가요?(숫자로 입력해주세요)');
-
-switch (parseInt(month)){
+let mt = prompt('현재는 몇 월인가요?(숫자로 입력해주세요.)');
+let month = parseInt(mt)
+console.log(typeof(month))
+if (month <= 0 || month > 12) alert('다시 입력해주세요.')
+switch (month){
     case 1: days = 31
     break;
     case 2: days = 29
@@ -22,10 +24,37 @@ switch (parseInt(month)){
 document.write('<table>');
 document.write(`<caption>${month}월</caption>`);
 document.write('<tr><th>일</th><th>월</th><th>화</th><th>수</th><th>목</th><th>금</th><th>토</th></tr>')
-for(i=0;i<days;i++){
-    if (i%7 == 0) document.write('<tr>');
-    document.write(`<td>${i+1}</td>`);
-    if (i%7 != 0 && i%14 == 0) {
-        document.write('</tr>');}
+for(i=0, k=1, l=0;i<5;i++){
+    document.write('<tr>');
+    for(j=0;j<7;j++){
+        document.write(`<td>`);
+        if ((month == 1 || month == 4 || month ==7) && l < 1){
+            document.write('')
+            l++;
+        }else if (month == 10 && l < 2){
+            document.write('')
+            l++;
+        }else if (month == 5 && l < 3){
+            document.write('')
+            l++;
+        }else if ((month == 2 || month == 8) && l < 4){
+            document.write('')
+            l++;
+        }else if ((month == 3 || month == 11) && l < 5){
+            document.write('')
+            l++;
+        }else if (month == 6 && l < 6){
+            document.write('')
+            l++;
+        }else if (k>days){
+            document.write('')
+        }
+        else{
+            document.write(`${k}`);
+            k++;
+        }
+        document.write(`</td>`);
+    }
+    document.write('</tr>');
 }
 document.write('</table>');
